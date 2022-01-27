b016fefb07a0e5c8aafc95d1ad7b899a32780de6
Andrea Scarpa <scarpandrea.1996@hotmail.com>
2022-01-27 12:30:40 +0100

    modif tasks migration

--- database/migrations/2022_01_27_112747_create_tasks_table.php
+++ database/migrations/2022_01_27_112747_create_tasks_table.php
@@ -15,6 +15,10 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string('Name');
+            $table->string('description');
+            $table->date('Data');
+
             $table->timestamps();
         });
     }
